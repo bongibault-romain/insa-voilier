9ca7e99d66faeeeb6940864c3f807381c8db73ac
Bongibault Romain <bongibault.romain@gmail.com>
2024-09-23 15:01:58 +0200

    feat: timer driver

--- Drivers/timer.c
+++ Drivers/timer.c
@@ -1,5 +1,18 @@
 #include "timer.h"
 
-void Timer_Enable(enum timer_name name) {
-	RCC->APB2ENR |= name;
+int __Timer_Get_Number(TIM_TypeDef *name) {
+	if (name == TIM1) return 11;
+	if (name == TIM2) return 0;
+	if (name == TIM3) return 1;
+	if (name == TIM4) return 2;
+	
+	return 0;
+}
+
+void Timer_Enable(TIM_TypeDef *name) {
+	if (name == TIM1) {
+		RCC->APB2ENR |= 1 << (__Timer_Get_Number(name));
+	} else {
+		RCC->APB1ENR |= 1 << (__Timer_Get_Number(name));
+	}
 }
--- Include/timer.h
+++ Include/timer.h
@@ -3,13 +3,6 @@
 
 #include "stm32f10x.h"
 
-enum timer_name {
-	IOPA = RCC_APB2ENR_IOPAEN,
-	IOPB = RCC_APB2ENR_IOPBEN,
-	IOPC = RCC_APB2ENR_IOPCEN,
-	IOPD = RCC_APB2ENR_IOPDEN
-};
-
-void Timer_Enable(enum timer_name name);
+void Timer_Enable(TIM_TypeDef *name);
 
 #endif
