26c10945d9136788926fdc0305ec55ce580593ef
Bongibault Romain <bongibault.romain@gmail.com>
2024-09-23 16:00:23 +0200

    feat: driver timer

--- Drivers/timer.c
+++ Drivers/timer.c
@@ -1,8 +1,20 @@
 #include "timer.h"
 
-void Timer_Enable(TIM_TypeDef *name) {
+void Timer_Enable(TIM_TypeDef *name, unsigned short psc, unsigned short arr) {
 	if (name == TIM1) RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
 	if (name == TIM2) RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 	if (name == TIM3) RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 	if (name == TIM4) RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+	
+	name->PSC = psc;
+	name->ARR = arr;	
+}
+
+
+void Timer_Start(TIM_TypeDef *name) {
+	name->CR1 |= 1;
+}
+
+void Timer_Stop(TIM_TypeDef *name) {
+	name->CR1 &= ~(1);
 }
