584a9b200fea50544e335abf7ce70f71e0380e9c
Bongibault Romain <bongibault.romain@gmail.com>
2024-11-04 15:28:01 +0100

    firmin1

--- Application/principal.c
+++ Application/principal.c
@@ -6,22 +6,24 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <time.h> 
 void test1() {
 	return;
 }
 
 int main (void)
 {
-	GPIO_Enable(GPIOA);
 	GPIO_Enable(GPIOB);
-	GPIO_Enable(GPIOC);
+	int arr=65535;
+	GPIO_Set_Config(TIM1_CHANNEL_1_GPIO, TIM1_CHANNEL_1_PIN, OUTPUT_2MHZ, O_ALTERNATE_GPO_PUSH_PULL); // GPIO_Set_Config(GPIO_TypeDef *name, char pin, enum gpio_mode mode, enum gpio_cnf config)
 	
-	Timer_Enable(TIM1, 548, 65535);
+	Timer_Enable(TIM1, 548, arr); //Timer_Enable(TIM_TypeDef *timer, unsigned short psc, unsigned short arr)
+	Timer_Start(TIM1); // Timer_Start(TIM_TypeDef *timer)
 	
-	GPIO_Set_Config(GPIOB, 0, INPUT, I_ANALOG);
 	
-	ADC_Enable(ADC1);
+	Timer_PWM_Enable(TIM1, TIM_CHANNEL_1, PWM_MODE_1); //Timer_PWM_Enable(TIM_TypeDef * timer, char channel, char mode)
+	Timer_PWM_Set(TIM1, TIM_CHANNEL_1, (arr+1)/11); // Timer_PWM_Set(TIM_TypeDef *timer, char channel, unsigned short value)
+	
 	
 	/* Tests for GPIO Driver */
 	/*
@@ -44,10 +46,13 @@ int main (void)
 	GPIO_Set_Config(GPIOB, 13, OUTPUT_50MHZ, O_ALTERNATE_GPO_OPEN_DRAIN);
 	GPIO_Set_Config(GPIOB, 14, OUTPUT_50MHZ, O_ALTERNATE_GPO_PUSH_PULL);
 	*/
-	
+	int i = 0;
 	while (1)
 	{
-		ADC_Read_Wait(ADC1);
+		for (int i = 0; i < 2147483647;i++){}
+		Timer_PWM_Set(TIM1, TIM_CHANNEL_1, (arr+1)/(10+i));
+		i=(i+1)%10;
 	}
+		
 }
 
