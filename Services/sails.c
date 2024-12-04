cfaa9e7be1b8d10e6ee61a94866225dd1e97e0b8
Anya Meetoo <anyameetoo@gmail.com>
2024-12-04 10:26:03 +0100

    calcul de l'angle sails

--- Services/sails.c
+++ Services/sails.c
@@ -11,10 +11,14 @@ void Sails_Enable(TIM_TypeDef * timer) {
 	Timer_PWM_Enable(timer, TIM_CHANNEL_1, PWM_MODE_1);
 }
 //int teta
-void Sails_Set(int theta_girouette){
+void Sails_Set(int theta){
+	Timer_PWM_Set(sails_timer, TIM_CHANNEL_1, (unsigned short) ((65535/26)/(SAILS_MIN - (SAILS_MIN-SAILS_MAX)*theta/90)));
+}
+
+int Angle_Giro_To_Angle_Sails (int theta_girouette){
 	int theta = 0;
 	if (theta_girouette > 45){
 		theta = ((90/135)*theta_girouette) - 30;
 	}
-	Timer_PWM_Set(sails_timer, TIM_CHANNEL_1, (unsigned short) ((65535/26)/(SAILS_MIN - (SAILS_MIN-SAILS_MAX)*theta/90)));
+	return theta;
 }
